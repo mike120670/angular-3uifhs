ddbf85ff598fc35f136408e6fcba0c1cd5d62bff
mike120670 <mike120670@gmail.com>
2022-11-07 14:57:01 -0800

    1 file modified

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -3,17 +3,26 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  styleUrls: ['./child.component.css'],
 })
 export class ChildComponent implements OnInit {
-  @Input()
+  /* @Input()
   arrList: Array<number> = [7,5,2,1,-3,9];
 
   @Output()
 
-  arrSelectedEventEmitter =  new EventEmitter();
+  arrSelectedEventEmitter =  new EventEmitter();*/
 
-  ngOnInit() {
+  @Input() msgFromParent: any[];
+  @Output() callParent = new EventEmitter();
+
+  name = 'Test 2';
+
+  currentMsgToParent = 'Test';
+
+  msgToParent() {
+    this.callParent.emit(this.currentMsgToParent);
   }
 
+  ngOnInit() {}
 }
