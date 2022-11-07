35a3bff46485169d40e5e8aee19a308a679e16fe
mike120670 <mike120670@gmail.com>
2022-11-07 14:34:48 -0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
+export class AppComponent {
+  name = 'Parent';
+  msgFromChild1: any;
+  currentMsgToChild2 = "I'm coming!";
+  msgToChild2 = [];
+  msg: any;
 
+  msgChild2() {
+    this.msgToChild2.push(this.currentMsgToChild2);
+  }
 
-export class AppComponent  {
-  name = 'Angular';
- 
-  constructor() { }
-
+  getMsgFromBaby($event) {
+    this.msg = $event;
+  }
 }
